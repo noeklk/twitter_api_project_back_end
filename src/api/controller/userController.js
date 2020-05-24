f3fc66da9298222f55a037d42ffa1d8768e4292c
Noé <noe.abdelkalek@gmail.com>
2020-05-24 23:05:27 +0200

    Fix userController

--- src/api/controller/userController.js
+++ src/api/controller/userController.js
@@ -5,27 +5,6 @@ const User = require("../model/userModel");
 const config = require("../../config");
 const { errorMessage } = config;
 
-exports.CreateAUser = (req, res) => {
-    try {
-        let new_user = new User(req.body);
-
-        new_user.save((error, user) => {
-            if (!error && user) {
-                res.status(201);
-                res.json(user);
-            } else {
-                res.status(400);
-                console.log(error);
-                res.json({ message: `L'utilisateur à l'identifiant: ${new_user.pseudo} existe déjà` });
-            }
-        })
-    } catch (e) {
-        res.status(500);
-        console.log(e);
-        res.json({ message: errorMessage })
-    }
-}
-
 exports.GetAllUsers = (req, res) => {
     try {
         User.find((error, users) => {
@@ -69,34 +48,23 @@ exports.GetAUserById = (req, res) => {
 
 exports.UserRegister = (req, res) => {
     try {
-        const { ADMIN_JWT_KEY } = process.env;
         let new_user = new User(req.body);
         const { password } = req.body;
-        const { role } = req.body;
-        const { admin_code } = req.body;
 
-        if (role === "admin" && admin_code != ADMIN_JWT_KEY) {
-            return res.status(400).json({ message: "Vous n'avez pas accès à la création de ce type de compte" })
-        } else {
-            new_user.admin_code = undefined;
+        let hashPass = bcrypt.hashSync(password, 10);
 
-            let hashPass = bcrypt.hashSync(password, 10);
+        new_user.password = hashPass;
 
-            new_user.password = hashPass;
-
-            new_user.save((error, user) => {
-                if (!error && user) {
-                    res.status(201);
-                    res.json({ message: "L'utilisateur a été corrèctement créé" });
-                } else {
-                    res.status(400);
-                    console.log(error);
-                    res.json({ message: errorMessage });
-                }
-            });
-        }
-
-
+        new_user.save((error, user) => {
+            if (!error && user) {
+                res.status(201);
+                res.json({ message: "L'utilisateur a été corrèctement créé" });
+            } else {
+                res.status(400);
+                console.log(error);
+                res.json({ message: errorMessage });
+            }
+        });
     }
     catch (e) {
         res.status(500);
