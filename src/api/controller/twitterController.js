66452dee1d0f0706b4cce82ca1f37fe8bd0614f2
Noé <noe.abdelkalek@gmail.com>
2020-05-25 18:25:35 +0200

    v1.3

--- src/api/controller/twitterController.js
+++ src/api/controller/twitterController.js
@@ -2,14 +2,7 @@ const express = require("express");
 const router = express.Router();
 const T = require("twit");
 
-router.get("/get_tweets", (req, res) => {
-
-    let accessToken = req.headers.accesstoken;
-    let accessTokenSecret = req.headers.accesstokensecret;
-
-    console.log(accessToken);
-    console.log(accessTokenSecret);
-
+function GenerateTwitClient(accessToken, accessTokenSecret) {
     const twit = new T({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -17,10 +10,20 @@ router.get("/get_tweets", (req, res) => {
         access_token_secret: accessTokenSecret
     });
 
+    return twit;
+}
+
+router.get("/get_tweets", (req, res) => {
+
+    let accessToken = req.headers.accesstoken;
+    let accessTokenSecret = req.headers.accesstokensecret;
+
+    const twit = GenerateTwitClient(accessToken, accessTokenSecret);
+
     twit.get("statuses/home_timeline", (err, data, response) => {
         console.log("data", data);
-        console.log("\nresponse", response);
-    })
+        // console.log("response", response);
+    });
 
     res.json({ test: "test" });
 });
