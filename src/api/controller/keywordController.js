5e7ef674b67e7fb4c8c664a72dfc8e30b80052ba
Noé <noe.abdelkalek@gmail.com>
2020-06-25 19:11:28 +0200

    Nettoyage et rangement de code

--- src/api/controller/keywordController.js
+++ src/api/controller/keywordController.js
@@ -1,4 +1,5 @@
 const Keyword = require("../model/keywordModel");
+const keywordHelper = require("../helper/keywordHelper");
 
 const config = require("../../config");
 const { errorMessage } = config;
@@ -7,9 +8,9 @@ const { errorMessage } = config;
 exports.GetAllKeywordsByKeyword = (req, res) => {
     try {
         let keyword = req.params.keyword;
-        const filterKey = req.query.filter ? req.query.filter : "";
-        let pipeline = getPipeline(filterKey, keyword);
-        
+        const filterKey = req.params.filter ? req.params.filter : "";
+        let pipeline = keywordHelper.GetPipeline(filterKey, keyword);
+
         Keyword.aggregate(pipeline, (error, data) => {
             if (!error) {
                 res.status(200);
@@ -21,7 +22,6 @@ exports.GetAllKeywordsByKeyword = (req, res) => {
                 res.json({ message: "Aucun mot trouvé" });
             }
         });
-
     } catch (e) {
         res.status(500);
         console.log(e);
@@ -44,8 +44,6 @@ exports.GetAKeywordById = (req, res) => {
                 res.json({ message: "Aucun mot trouvé" });
             }
         })
-
-        // res.json({keyword_id});
     } catch (e) {
         res.status(500);
         console.log(e);
@@ -69,8 +67,6 @@ exports.UpdateAKeywordById = (req, res) => {
                 res.json({ message: "Aucun mot trouvé" });
             }
         })
-
-        // res.json({keyword_id});
     } catch (e) {
         res.status(500);
         console.log(e);
@@ -93,75 +89,9 @@ exports.DeleteAKeywordById = (req, res) => {
                 res.json({ message: "Aucun mot trouvé" });
             }
         })
-
-        // res.json({keyword_id});
     } catch (e) {
         res.status(500);
         console.log(e);
         res.json({ message: errorMessage });
     }
-}
-
-function getPipeline(filterKey, keyword) {
-
-    // Retourne les stats des dernieres 24h grouper par heure (totale 24)
-    if (filterKey == "hourly"){
-        return [
-            { $match: {keyword: keyword , created_at : { $gt: new Date(Date.now() - 24*60*60*1000) } } },
-            { $sort: { created_at: 1 } },
-            { 
-                $group: {
-                    _id: { "$hour": "$created_at" },
-                    first: {"$first": "$$ROOT"},
-                } 
-            }
-        ];
-    }
-    // Retourne les stats des dernieres 7j grouper par jour (totale 7)
-    else if (filterKey == "daily") {
-        return [
-            { $match: {keyword: keyword , created_at : { $gt: new Date(Date.now() - 7*24*60*60*1000) } } },
-            { $sort: { created_at: 1 } },
-            { 
-                $group: {
-                    _id: { "$dayOfMonth": "$created_at" },
-                    first: {"$first": "$$ROOT"},
-                } 
-            }
-        ];
-    }
-    // Retourne les stats de la l'anneé grouper par mois (totale 12)
-    else if (filterKey == "monthly") {
-        return [
-            { $match: {keyword: keyword , created_at : { $gt: new Date(Date.now() - 365*24*60*60*1000) } } },
-            { $sort: { created_at: 1 } },
-            { 
-                $group: {
-                    _id: { "$month": "$created_at" },
-                    first: {"$first": "$$ROOT"},
-                } 
-            }
-        ];
-    }
-    // Retourne les stats de la derniere heure grouper par 2minutes (totale 30)
-    else {
-        return [
-            { $match: {keyword: keyword , created_at : { $gt: new Date(Date.now() - 60*60*1000) } } },
-            { $sort: { created_at: 1 } },
-            { 
-                $group: {
-                    _id: {
-                        "$toDate": {
-                            "$subtract": [
-                                { "$toLong": { "$toDate": "$_id" }  },
-                                { "$mod": [ { "$toLong": { "$toDate": "$_id" } }, 1000 * 60 * 2 ] }
-                            ]
-                        }
-                    },
-                    first: {"$first": "$$ROOT"},
-                } 
-            }
-        ];
-    }
-    
 }
